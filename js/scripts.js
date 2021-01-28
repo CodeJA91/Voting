e8e4f93179e7cbaa2122047e536d54f2bf1f3fe9
CodeJA91 <mirroredperspective@protonmail.com>
2021-01-27 20:28:57 -0800

    Added boilerplate, basic html, styling, and script funtionality
    Co-authored-by: Benji Brucker <benjamin.brucker@gmail.com
    Co-authored-by: Daisy Collins <daisydcollins12@gmail.com>
    Co-authored-by: Benji Brucker <benjamin.brucker@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){}
 
 const age = parseInt(prompt("How old are you?"));
 
-  if (age >= 18) {
+ if (age >= 18) {
     $('#can-vote').show();
   }else{
     $('#no-vote').show();
