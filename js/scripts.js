e8cdc476fac2d1e55e1fdd89a7e8947d43e6664a
CodeJA91 <mirroredperspective@protonmail.com>
2021-01-27 20:49:06 -0800

    Added further interactivity.
    Co-authored-by: Karlson Drendel <kdrendel99@gmail.com>
    Co-authored-by: Daisy Collins <daisydcollins12@gmail.com>
    Co-authored-by: Benji Brucker <benjamin.brucker@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){}
+$(document).ready(function(){
 
 
 const age = parseInt(prompt("How old are you?"));
 
- if (age >= 18) {
+  if (age >= 18) {
     $('#can-vote').show();
   }else{
     $('#no-vote').show();
