5ce7ccdf0325731c3c8b5464fbd58789e41925da
CodeJA91 <mirroredperspective@protonmail.com>
2021-01-27 20:16:56 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){}
+
+
+const age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $('#can-vote').show();
+  }else{
+    $('#no-vote').show();
+  }
+});
